11a473998e7f480af9a991c071917cc99e06fede
Jared Parnell <jared.parnell@opendataservices.coop>
2019-11-21 17:06:14 +0000

    superficial face-lift part 1 - PWYF branding

--- ActivityMerger/__init__.py
+++ ActivityMerger/__init__.py
@@ -1,6 +1,7 @@
 import os, uuid, shutil
 from flask import Flask, request, redirect, url_for, abort
 from flask import send_from_directory, render_template
+from flask_assets import Environment as FlaskAssets, Bundle
 from werkzeug.utils import secure_filename
 from ActivityMerger import merge
 
@@ -20,6 +21,25 @@ app.config.from_mapping(
     OUTPUT_FOLDER='output'
 )
 
+assets = FlaskAssets(app)
+assets.register('js_base', Bundle(
+    os.path.join('js', 'jquery-1.12.4.js'),
+    os.path.join('js', 'bootstrap-3.3.7.js'),
+    filters='jsmin',
+    output=os.path.join('gen', 'js.%(version)s.min.js'))
+)
+assets.register('js_activity', Bundle(
+    os.path.join('js', 'activity.js'),
+    filters='jsmin',
+    output=os.path.join('gen', 'activity.%(version)s.min.js'))
+)
+assets.register('css_base', Bundle(
+    os.path.join('css', 'bootstrap.css'),
+    os.path.join('css', 'font-awesome.css'),
+    os.path.join('css', 'app.css'),
+    filters='cssmin',
+    output=os.path.join('gen', 'css.%(version)s.min.css'))
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
