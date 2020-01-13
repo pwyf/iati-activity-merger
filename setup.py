536626c625351f9db140090ac211e97176b57d74
Jared Parnell <jared.parnell@opendataservices.coop>
2020-01-13 14:24:33 +0000

    admin: more useful project setup(.py)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='activitymerger',
+    version='1.0.0',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    packages=['ActivityMerger'],
+    url='https://github.com/OpenDataServices/iati-activity-merger',
+    license='MIT',
+    description='A tool for merging IATI activity files into a single XML file',
+)
